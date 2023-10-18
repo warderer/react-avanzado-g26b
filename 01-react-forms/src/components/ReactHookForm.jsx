523eae441dff228e9d93d11a17be366dcc22cb4b
César Guerra <warderer@gmail.com>
2023-10-17 21:50:34 -0600

    ✨ ReactHookForm validado por Yup

--- 01-react-forms/src/components/ReactHookForm.jsx
+++ 01-react-forms/src/components/ReactHookForm.jsx
@@ -1,12 +1,31 @@
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import logo from '../assets/react.svg'
 
 const ReactHookForm = () => {
+  const schema = yup.object({
+    firstName: yup.string().required('Escribe tu nombre'),
+    lastName: yup.string().required('Escribe tu apellido'),
+    age: yup.number().positive('La edad debe ser un número positivo').integer('Indica tu edad'),
+    email: yup.string().email('Correo invalido').required('Por favor indica un correo'),
+    password: yup.string().required('No password provided.')
+      .min(8, 'Password is too short - should be 8 chars minimum.')
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%.^&*])/, 'La contraseña debe tener al menos 8 caracteres, un número, una letra mayúscula, una letra minúscula y un caracter especial'),
+    genre: yup
+      .mixed()
+      .oneOf(['M', 'F', 'O'], 'Selecciona un genero: Hombre, Mujer u Otro')
+      .defined()
+  })
+    .required()
+
   const sendData = (data) => {
     console.log('Send data', data)
   }
 
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
 
   return (
     <div className='login'>
@@ -23,8 +42,9 @@ const ReactHookForm = () => {
             name='firstName'
             placeholder='Tu Nombre'
             id='firstName'
-            {...register('firstName')}
+            {...register('firstName', { required: true, maxLength: 20 })}
           />
+          {errors.firstName && <p>{errors.firstName?.message}</p>}
 
           <label htmlFor='lastName'>Apellido</label>
           <input
@@ -32,8 +52,9 @@ const ReactHookForm = () => {
             name='lastName'
             placeholder='Tu Apellido'
             id='lastName'
-            {...register('lastName')}
+            {...register('lastName', { pattern: /^[A-Za-z]+$/i })}
           />
+          {errors.lastName && <p>{errors.lastName?.message}</p>}
 
           <label htmlFor='age'>Edad</label>
           <input
@@ -43,6 +64,7 @@ const ReactHookForm = () => {
             id='age'
             {...register('age')}
           />
+          {errors.age && <p>{errors.age?.message}</p>}
 
           <label htmlFor='gender'>Genero</label>
           <select name='gender' id='gender' {...register('gender')}>
@@ -51,6 +73,7 @@ const ReactHookForm = () => {
             <option value='F'>Femenino</option>
             <option value='O'>Otro</option>
           </select>
+          {errors.gender && <p>{errors.gender?.message}</p>}
 
           <label htmlFor='email'>Email</label>
           <input
@@ -60,6 +83,7 @@ const ReactHookForm = () => {
             id='email'
             {...register('email')}
           />
+          {errors.email && <p>{errors.email?.message}</p>}
 
           <label htmlFor='password'>Password</label>
           <input
@@ -68,6 +92,7 @@ const ReactHookForm = () => {
             id='password'
             {...register('password')}
           />
+          {errors.password && <p>{errors.password?.message}</p>}
 
           <button type='submit'>
             Iniciar Sesion
