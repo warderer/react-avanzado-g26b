8cad2c8463576f0ecec6b0e82352de50db3bbb96
César Guerra <warderer@gmail.com>
2023-10-19 21:09:18 -0600

    ✨ SongContext y Provider

---
+++ 02-react-context/src/context/SongContext.jsx
@@ -0,0 +1,49 @@
+import { createContext, useState, useEffect } from 'react'
+// Usamos @ (alias) para no tener que poner ../../
+// import canciones from '../../assets/listaCanciones.json'
+import canciones from '@/assets/listaCanciones.json'
+
+// CONTEXT tiene que ver con el manejo de estados globales en React.
+// Es decir, poder compartir LA MISMA información entre diferentes NIVELES de componentes.
+
+// Para usar CONTEXT necesitamos seguir una serie de pasos:
+
+// 1. Crear el contexto
+const SongContext = createContext()
+
+// 2. Crear el proveedor del contexto
+// Es decir, maneja de donde se obtiene la información y como se comparte.
+// El proveedor es un componente de React que envuelve a los componentes que van a usar el contexto.
+
+function SongProvider ({ children }) {
+  const [list, setList] = useState([]) // lista de canciones
+  const [loading, setLoading] = useState(true) // ¿esta cargando la lista?
+  const [selectedSong, setSelectedSong] = useState({}) // canción seleccionada
+
+  // Simulo mi llamada a la API
+  useEffect(() => {
+    setTimeout(() => {
+      setList(canciones)
+      setLoading(false)
+    }, 2000)
+  }, [])
+
+  const data = {
+    list,
+    loading,
+    selectedSong,
+    setSelectedSong
+  }
+
+  return (
+    <SongContext.Provider value={data}>
+      {children}
+    </SongContext.Provider>
+  )
+}
+
+// Exportar las funciones del Provider y el Contexto para hacerlos accesibles.
+export {
+  SongProvider,
+  SongContext
+}
--- 02-react-context/src/pages/Home/Home.jsx
+++ 02-react-context/src/pages/Home/Home.jsx
@@ -1,19 +1,22 @@
 import SongList from '@/components/SongList/SongList'
 import SongDetails from '@/components/SongDetails/SongDetails'
+import { SongProvider } from '@/context/SongContext'
 import './home.css'
 
 const Home = () => {
   return (
-    <div className='home-container'>
-      <div className='izquierdo'>
-        <h2>Lado Izquierdo</h2>
-        <SongList />
+    <SongProvider>
+      <div className='home-container'>
+        <div className='izquierdo'>
+          <h2>Lado Izquierdo</h2>
+          <SongList />
+        </div>
+        <div className='derecho'>
+          <h2>Lado Derecho</h2>
+          <SongDetails />
+        </div>
       </div>
-      <div className='derecho'>
-        <h2>Lado Derecho</h2>
-        <SongDetails />
-      </div>
-    </div>
+    </SongProvider>
   )
 }
 export default Home
