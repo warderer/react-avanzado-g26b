32b621ab39f48dbc2db5cc2cdab03ecb9e01c8c2
César Guerra <warderer@gmail.com>
2023-10-19 21:31:25 -0600

    ✨ Seleccionar cancion en SongList y verlo en SongDetails

--- 02-react-context/src/components/SongDetails/SongDetails.jsx
+++ 02-react-context/src/components/SongDetails/SongDetails.jsx
@@ -1,6 +1,23 @@
+import { useSongContext } from '@/hooks/useSongContext'
+
 const SongDetails = () => {
+  const { selectedSong } = useSongContext()
+
   return (
-    <h1>El detalle de la canción que seleccionaste</h1>
+    <>
+      {
+        selectedSong.title
+          ? (
+            <div>
+              <img src={selectedSong.img_url} alt={selectedSong.title} />
+              <h2>{selectedSong.title}</h2>
+              <h3>{selectedSong.artist}</h3>
+              <h6>{selectedSong.year}</h6>
+            </div>
+            )
+          : <h1>Selecciona una canción</h1>
+        }
+    </>
   )
 }
 export default SongDetails
--- 02-react-context/src/components/SongList/SongList.jsx
+++ 02-react-context/src/components/SongList/SongList.jsx
@@ -2,14 +2,18 @@ import { useSongContext } from '@/hooks/useSongContext'
 import './songlist.css'
 
 const SongList = () => {
-  const { list, loading } = useSongContext()
+  const { list, loading, setSelectedSong } = useSongContext()
 
   return (
     <section className='row-container'>
       {loading
         ? <h1>Cargando...</h1>
         : list.map((song) => (
-          <div className='row-song' key={song.id}>
+          <div
+            className='row-song'
+            key={song.id}
+            onClick={() => setSelectedSong(song)}
+          >
             <h3>{song.title}</h3>
             <h4>{song.artist}</h4>
           </div>
