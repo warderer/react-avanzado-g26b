25e86f543f2115008e1365a599155145700c7ca2
César Guerra <warderer@gmail.com>
2023-10-24 20:23:08 -0600

    🚨 Config de linter con standard js

--- 03-react-auth/.eslintrc.cjs
+++ 03-react-auth/.eslintrc.cjs
@@ -6,6 +6,8 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'standard',
+    'standard-jsx'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -16,5 +18,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off',
   },
 }
