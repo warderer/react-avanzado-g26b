7f183ff14490a3b8956e22e054f81da4e72d8447
César Guerra <warderer@gmail.com>
2023-10-24 21:14:41 -0600

    ✨ Estructura del Menu

--- 03-react-auth/src/components/Header/Header.jsx
+++ 03-react-auth/src/components/Header/Header.jsx
@@ -2,7 +2,26 @@ import './header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className='header'>
+      <a className='header__logo' href='/'>LOGO</a>
+      <ul className='header__nav-list'>
+        <li className='header__list-item'>
+          <a href='/' className='header__item-link header__item-link--is-active'>Home</a>
+        </li>
+        <li className='header__list-item'>
+          <a href='/dashboard' className='header__item-link'>Dashboard</a>
+        </li>
+        <li className='header__list-item'>
+          <a href='/secret' className='header__item-link'>Secret</a>
+        </li>
+        <li className='header__list-item'>
+          <a href='/login' className='header__item-link'>Login</a>
+        </li>
+        <li className='header__list-item'>
+          <a href='/signup' className='header__item-link'>Signup</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 export default Header
