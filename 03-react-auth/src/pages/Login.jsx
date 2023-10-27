e29a43188518b9102495e67fb3570d62eeda6ed9
César Guerra <warderer@gmail.com>
2023-10-26 20:37:01 -0600

    💄 UI de Login

--- 03-react-auth/src/pages/Login.jsx
+++ 03-react-auth/src/pages/Login.jsx
@@ -1,6 +1,44 @@
+import logo from '@/assets/react.svg'
+import '@/styles/form.css'
+
 const Login = () => {
   return (
-    <h1>Login</h1>
+    <main className='form-signin w-100 m-auto'>
+      <form>
+        <img
+          className='mb-4'
+          src={logo}
+          alt=''
+          width={72}
+          height={57}
+        />
+        <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+        <div className='form-floating'>
+          <input
+            type='email'
+            className='form-control'
+            id='floatingInput'
+            placeholder='name@example.com'
+          />
+          <label htmlFor='floatingInput'>Email address</label>
+        </div>
+        <div className='form-floating'>
+          <input
+            type='password'
+            className='form-control'
+            id='floatingPassword'
+            placeholder='Password'
+          />
+          <label htmlFor='floatingPassword'>Password</label>
+        </div>
+
+        <button className='btn btn-primary w-100 py-2' type='submit'>
+          Sign in
+        </button>
+        <p className='mt-5 mb-3 text-body-secondary'>© 2017–2023</p>
+      </form>
+    </main>
+
   )
 }
 export default Login
