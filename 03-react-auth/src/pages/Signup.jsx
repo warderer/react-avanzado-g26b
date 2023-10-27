5a025650be30fccbf4a438d3be7ee8c06c23e9ce
César Guerra <warderer@gmail.com>
2023-10-26 20:56:58 -0600

    ✨ Signup con react-hook-form

--- 03-react-auth/src/pages/Signup.jsx
+++ 03-react-auth/src/pages/Signup.jsx
@@ -1,10 +1,19 @@
+import { useForm } from 'react-hook-form'
 import logo from '@/assets/react.svg'
 import '@/styles/form.css'
 
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
   return (
     <main className='form-signin w-100 m-auto'>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <img className='mb-4' src={logo} alt='' width='72' height='57' />
         <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
 
@@ -15,9 +24,11 @@ const Signup = () => {
             id='first_name'
             name='first_name'
             placeholder='John'
+            {...register('first_name', { required: true })}
           />
           <label htmlFor='first_name'>First Name</label>
         </div>
+        {errors.first_name && <span>This field is required</span>}
 
         <div className='form-floating'>
           <input
@@ -26,15 +37,18 @@ const Signup = () => {
             id='last_name'
             name='last_name'
             placeholder='Doe'
+            {...register('last_name', { required: true })}
           />
           <label htmlFor='last_name'>Last Name</label>
         </div>
+        {errors.last_name && <span>This field is required</span>}
 
         <div className='form-floating'>
           <select
             className='form-select'
             id='gender'
             name='gender'
+            {...register('gender')}
           >
             <option value=''>Choose...</option>
             <option value='M'>Male</option>
@@ -50,9 +64,11 @@ const Signup = () => {
             id='email'
             name='email'
             placeholder='name@example.com'
+            {...register('email', { required: true })}
           />
           <label htmlFor='email'>Email address</label>
         </div>
+        {errors.email && <span>This field is required</span>}
 
         <div className='form-floating'>
           <input
@@ -61,9 +77,11 @@ const Signup = () => {
             id='password'
             name='password'
             placeholder='Password'
+            {...register('password', { required: true })}
           />
           <label htmlFor='password'>Password</label>
         </div>
+        {errors.password && <span>This field is required</span>}
 
         <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign up</button>
         <p className='mt-5 mb-3 text-muted'>© 2017–2022</p>
