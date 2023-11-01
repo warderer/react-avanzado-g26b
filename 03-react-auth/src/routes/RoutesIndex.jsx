a0e72b98bebd1fb821f28ecfc727596dbebe55f4
César Guerra <warderer@gmail.com>
2023-10-31 20:38:52 -0600

    ✨ Protegiendo Rutas

--- 03-react-auth/src/routes/RoutesIndex.jsx
+++ 03-react-auth/src/routes/RoutesIndex.jsx
@@ -1,14 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Dashboard, Home, Login, Secret, Signup } from '@/pages'
+import { useAuthContext } from '@/hooks/useAuthContext'
 
 const RoutesIndex = () => {
+  const { isAuth } = useAuthContext()
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='/secret' element={<Secret />} />
+      <Route
+        path='/dashboard' element={
+        isAuth ? <Dashboard /> : <Navigate to='/login' />
+      }
+      />
+      <Route
+        path='/secret' element={
+          isAuth ? <Secret /> : <Navigate to='/login' />
+      }
+      />
     </Routes>
   )
 }
