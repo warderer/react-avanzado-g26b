f8d107217900d15dc2103acd31ef3a83909bdea8
César Guerra <warderer@gmail.com>
2023-11-07 19:53:56 -0600

    🔧 cypress baseUrl

--- 03-react-auth/cypress.config.js
+++ 03-react-auth/cypress.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents (on, config) {
       // implement node event listeners here
-    }
+    },
+    baseUrl: 'http://localhost:5173'
   }
 })
